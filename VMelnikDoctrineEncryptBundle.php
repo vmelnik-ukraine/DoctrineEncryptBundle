831d46e10201a8c5bc09f6a28411837d52c77925
Marius Krämer <marius.kraemer@mercoline.de>
2012-12-18 15:33:22 +0100

    Initialization of the extension now compatible with symfony 2.1

--- VMelnikDoctrineEncryptBundle.php
+++ VMelnikDoctrineEncryptBundle.php
@@ -3,7 +3,13 @@
 namespace VMelnik\DoctrineEncryptBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use VMelnik\DoctrineEncryptBundle\DependencyInjection\VMelnikDoctrineEncryptExtension;
+
 
 class VMelnikDoctrineEncryptBundle extends Bundle {
-    
+    public function getContainerExtension()
+    {
+        return new VMelnikDoctrineEncryptExtension();
+    }
 }
