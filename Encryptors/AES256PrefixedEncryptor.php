7a17cf61ca873c4d70735135780f30c60311cb33
Westin Pigott <wpigott@tdmobility.com>
2013-10-02 15:56:18 -0500

    #2 - Fixing the stuff that didnt work

--- Encryptors/AES256PrefixedEncryptor.php
+++ Encryptors/AES256PrefixedEncryptor.php
@@ -25,11 +25,18 @@ class AES256PrefixedEncryptor implements EncryptorInterface {
     private $secretKey;
 
     /**
+     *
+     * @var int
+     */
+    private $iv_size;
+
+    /**
      * Initialization of encryptor
      * @param string $key 
      */
     public function __construct($key) {
         $this->secretKey = $this->convertKey($key);
+        $this->iv_size = mcrypt_get_iv_size(self::CIPHER, self::MODE);
     }
 
     /**
@@ -46,14 +53,14 @@ class AES256PrefixedEncryptor implements EncryptorInterface {
      * @return string
      */
     public function encrypt($data) {
-        // Generate Initialization Vector
-        $iv = $this->generateInitializationVector();
+        // Return an initialization vector (IV) from a random source
+        $iv = mcrypt_create_iv($this->iv_size, MCRYPT_DEV_URANDOM);
 
         // Encrypt plaintext data with given parameters
         $encrypted = mcrypt_encrypt(self::CIPHER, $this->secretKey, $data, self::MODE, $iv);
 
         // Encode data with MIME base64
-        $base64_encoded = base64_encode($encrypted);
+        $base64_encoded = base64_encode($iv . $encrypted);
 
         // Strip NULL-bytes from the end of the string
         $rtrimmed = rtrim($base64_encoded, "\0");
@@ -72,16 +79,17 @@ class AES256PrefixedEncryptor implements EncryptorInterface {
             return $data;
 
         // Strip annotation from data
-        $annotation_removed = str_replace($this->prefix, '', $data, 1);
-
-        // Generate Initialization Vector
-        $iv = $this->generateInitializationVector();
+        $annotation_removed = substr($data, strlen($this->prefix));
 
         // Decode data encoded with MIME base64
         $base64_decoded = base64_decode($annotation_removed);
 
+        // Split Initialization Vector
+        $iv = substr($base64_decoded, 0, $this->iv_size);
+        $iv_removed = substr($base64_decoded, $this->iv_size);
+
         // Decrypt crypttext with given parameters
-        $decrypted = mcrypt_decrypt(self::CIPHER, $this->secretKey, $base64_decoded, self::MODE, $iv);
+        $decrypted = mcrypt_decrypt(self::CIPHER, $this->secretKey, $iv_removed, self::MODE, $iv);
 
         // Strip NULL-bytes from the end of the string and return
         return rtrim($decrypted, "\0");
@@ -94,14 +102,6 @@ class AES256PrefixedEncryptor implements EncryptorInterface {
      */
     private function convertKey($secretKey) {
         return pack('H*', hash('sha256', $secretKey));
-    }
-
-    private function generateInitializationVector() {
-        // Get the size of the Initialization Vector (IV) belonging to a specific cipher/mode combination
-        $iv_size = mcrypt_get_iv_size(self::CIPHER, self::MODE);
-
-        // Return an initialization vector (IV) from a random source
-        return mcrypt_create_iv($iv_size, MCRYPT_DEV_URANDOM);
     }
 
 }
